c5acc2692f5394baa886f71324d8dfe53b7299fe
niklaus <alwaysandforever@theoriginals.com>
2020-01-22 21:57:10 +0800

    update

--- cloud_computer_initial_settings.sh
+++ cloud_computer_initial_settings.sh
@@ -1,6 +1,6 @@
+apt-get install neofetch
 git config --global --user.name "niiiklaus"
 git config --global --user.email "1372063766@qq.com"
-apt-get install neofetch
 alias c="clear"
 alias s="neofetch"
 alias l="ls -Al"
