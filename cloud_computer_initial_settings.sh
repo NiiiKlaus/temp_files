48209082112193d85c22a6add1539db64101c4ea
niiiklaus <1372063766@qq.com>
2020-01-22 14:15:59 +0000

    update

--- cloud_computer_initial_settings.sh
+++ cloud_computer_initial_settings.sh
@@ -1,6 +1,6 @@
 apt-get install neofetch
-git config --global --user.name "niiiklaus"
-git config --global --user.email "1372063766@qq.com"
+git config --global user.name "niiiklaus"
+git config --global user.email "1372063766@qq.com"
 alias c="clear"
 alias s="neofetch"
 alias l="ls -Al"
