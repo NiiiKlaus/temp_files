adc6f79038a249f095bfc96ac4e662c278e31a12
niklaus <alwaysandforever@theoriginals.com>
2020-01-22 23:20:51 +0800

    update

--- cloud_computer_initial_settings.sh
+++ cloud_computer_initial_settings.sh
@@ -1,7 +1,11 @@
+# basic
 apt-get install neofetch
 git config --global user.name "niiiklaus"
 git config --global user.email "1372063766@qq.com"
 alias c="clear"
 alias s="neofetch"
 alias l="ls -Al"
+
+# Jupyter notebook Settings
+cp ~/temp_files/jupyter_notebook_config.py ~/.jupyter
 
