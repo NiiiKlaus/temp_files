05ba79ab8c315c26ffa7cff3bd14d68e7f9b6915
niklaus <alwaysandforever@theoriginals.com>
2020-01-23 18:53:45 +0800

    update

--- initiate.sh
+++ initiate.sh
@@ -1,5 +1,14 @@
-# install apps
+sudo_commands=(
 apt-get install neofetch
+)
+
+if [ $(whoami) = root ]
+then
+    for command in ${sudo_commands[@]}
+        do
+            $command
+        done
+fi
 
 # set up git
 git config --global user.name "niiiklaus"
@@ -8,3 +17,11 @@ git config --global user.email "1372063766@qq.com"
 # source scripts
 source ~/temp_files/scripts/*.sh
 
+# at last
+echo "commands to be executed:"
+echo
+for command in ${sudo_commands[@]}
+do
+    echo "sudo $command"
+    echo
+done
