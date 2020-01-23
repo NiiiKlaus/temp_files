fafa24bc22183170d6c551fc7dcb552abf1a5d05
niklaus <alwaysandforever@theoriginals.com>
2020-01-23 18:33:47 +0800

    update

---
+++ initiate.sh
@@ -0,0 +1,10 @@
+# install apps
+apt-get install neofetch
+
+# set up git
+git config --global user.name "niiiklaus"
+git config --global user.email "1372063766@qq.com"
+
+# source scripts
+source ~/temp_files/scripts/*.sh
+
---
+++ scripts/aliases.sh
@@ -0,0 +1,7 @@
+alias c="clear"
+alias s="neofetch"
+alias g="git pull"
+alias l="ls -Al"
+alias h="cd ~"
+
+alias myip="curl -4sSkL https://myip.ipip.net"
